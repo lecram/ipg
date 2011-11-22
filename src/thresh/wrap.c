0588f04a618ac7f8111a9f1840a16c0d8035b4b0
Marcel Rodrigues <marcelgmr@gmail.com>
2011-11-22 09:09:05 -0200

    Fixed argument parsing.

--- src/thresh/wrap.c
+++ src/thresh/wrap.c
@@ -7,21 +7,21 @@
 void
 proc(Matrix *matrix, Args *args)
 {
-    float threshold, below, above;
+    double threshold, below, above;
     unsigned char uc_threshold, uc_below, uc_above;
     int keep;
 
     below = above = 0.0;
     keep = KEEP_NONE;
-    threshold = strtof(args->v[0], NULL);
+    threshold = strtod(args->v[0], NULL);
     if (!strcmp(args->v[1], "="))
         keep |= KEEP_BELOW;
     else
-        below = strtof(args->v[1], NULL);
+        below = strtod(args->v[1], NULL);
     if (!strcmp(args->v[2], "="))
         keep |= KEEP_ABOVE;
     else
-        above = strtof(args->v[2], NULL);
+        above = strtod(args->v[2], NULL);
     uc_threshold = (unsigned char) (threshold * 255.0);
     uc_below = (unsigned char) (below * 255.0);
     uc_above = (unsigned char) (above * 255.0);
