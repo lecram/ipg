18e04082d2212f0001d42ec354cb217a4f329793
Marcel Rodrigues <marcelgmr@gmail.com>
2011-11-22 16:19:08 -0200

    Multiple frame processing on GPU.

--- src/thresh/trans.cu
+++ src/thresh/trans.cu
@@ -48,3 +48,56 @@ thresh(Matrix *matrix, unsigned char threshold,
     cudaMemcpy(matrix->data, d_img, size, cudaMemcpyDeviceToHost);
     cudaFree(d_img);
 }
+
+__global__ void
+thresh_pixelN(unsigned char *img, int width, int height,
+              unsigned char threshold,
+              unsigned char below, unsigned char above,
+              int keep)
+{
+    int x, y, z;
+    int offset;
+    int size;
+
+    size = width * height;
+    x = blockIdx.x * blockDim.x + threadIdx.x;
+    y = blockIdx.y * blockDim.y + threadIdx.y;
+    offset = y * width + x;
+    z = offset / size;
+    x %= width;
+    y %= height;
+    if (z >= N)
+        return;
+
+
+    if (img[offset] <= threshold) {
+        if (!(keep & KEEP_BELOW))
+            img[offset] = below;
+    }
+    else {
+        if (!(keep & KEEP_ABOVE))
+            img[offset] = above;
+    }
+}
+
+extern "C"
+void
+threshN(Matrix *matrix, unsigned char threshold,
+        unsigned char below, unsigned char above,
+        int keep)
+{
+    unsigned char *d_img;
+    size_t size;
+    dim3 threadsPerBlock(16, 16);
+    dim3 numBlocks((matrix->width * N + threadsPerBlock.x - 1) / threadsPerBlock.x,
+                   (matrix->height * N + threadsPerBlock.y - 1) / threadsPerBlock.y);
+
+    size = matrix->width * matrix->height * N;
+    cudaMalloc((void **) &d_img, size);
+    cudaMemcpy(d_img, matrix->data, size, cudaMemcpyHostToDevice);
+    thresh_pixelN<<<numBlocks, threadsPerBlock>>>(
+        d_img, matrix->width, matrix->height, threshold, below, above, keep
+    );
+    cudaMemcpy(matrix->data, d_img, size, cudaMemcpyDeviceToHost);
+    cudaFree(d_img);
+}
