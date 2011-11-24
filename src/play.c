bf7da3bdffd0327eeb0abd79656f4bb4fd23755c
Marcel Rodrigues <marcelgmr@gmail.com>
2011-11-24 14:12:27 -0200

    Added minimal video player.

---
+++ src/play.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <opencv/cv.h>
+#include <opencv/highgui.h>
+
+
+int
+main(int argc, char *argv[])
+{
+    CvCapture *capture = NULL;
+    IplImage *frame;
+    char *infile;
+    double fps;
+    double wait;
+
+    infile = argv[1];
+    capture = cvCaptureFromFile(infile);
+    if (capture == NULL) {
+        printf("Could not load video \"%s\".\n", infile);
+        return EXIT_FAILURE;
+    }
+    fps = cvGetCaptureProperty(capture, CV_CAP_PROP_FPS);
+    wait = 1000 / fps;
+    cvNamedWindow(infile, 1);
+    while ((frame = cvQueryFrame(capture)) != NULL) {
+        cvShowImage(infile, frame);
+        cvWaitKey(wait);
+    }
+    cvReleaseCapture(&capture);
+    cvDestroyWindow(infile);
+    return EXIT_SUCCESS;
+}
